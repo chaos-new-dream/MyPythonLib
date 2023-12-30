4090bdb475fbe2f0c4d83b99637ccc7565498b53
chaos-new-dream~ <1938679111@qq.com>
2023-12-31 02:24:40 +0800

    删除

--- 屏幕亮度变50.py
+++
@@ -1,9 +0,0 @@
-# import wmi
-# c = wmi.WMI(namespace='root\WMI')
- 
-# a = c.WmiMonitorBrightnessMethods()[0]
-# a.WmiSetBrightness(Brightness=80, Timeout=500) # Brightness：亮度
-
-import screen_brightness_control as sbc
-
-sbc.set_brightness(50)
